dac5f3c1b8e0e3082ebb412bd85323509c2f6da3
savinaysuresh <savinaysuresh13@gmail.com>
2025-10-22 13:51:14 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,14 @@
-// client/src/components/Header.jsx
 import React from "react";
 import Navbar from "./Navbar";
-import NotificationBell from "./NotificationBell"; // 1. Import the bell
+import NotificationBell from "./NotificationBell";
 
 const Header = () => (
-  // 2. Add layout and styling to the header
   <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
     
-    {/* Left side: Title */}
+    {}
     <h1 className="text-xl font-bold">FoundIt</h1>
     
-    {/* Right side: Group the Navbar and Bell */}
+    {}
     <div className="flex items-center space-x-4">
       <Navbar /> 
       <NotificationBell />
